284e3c8c76b8dd1df037f46515bbc9d48b667bd2
= <=>
2022-04-11 18:07:44 -0500

    Versión conteniendo registrar, listar y búsqueda de Productos

--- src/main/java/app/FrmManteProd.java
+++ src/main/java/app/FrmManteProd.java
@@ -143,9 +143,32 @@ public class FrmManteProd extends JFrame {
 		cboProveedores.setBounds(310, 106, 86, 22);
 		contentPane.add(cboProveedores);
 		
+		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				buscarProducto();
+			}
+		});
+		btnBuscar.setBounds(324, 54, 89, 23);
+		contentPane.add(btnBuscar);
+		
 		llenaCombo();
 	}
 
+	void buscarProducto() {
+		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("mysql");
+		EntityManager em = fabrica.createEntityManager();
+		
+		Producto p = em.find(Producto.class, txtCódigo.getText()); 
+
+		if (p == null)
+			txtSalida.setText("Código no existe...!");
+		else {
+			txtDescripcion.setText(p.getDes_prod());
+		}
+		em.close();
+	}
+	
 	void llenaCombo() {
 		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("mysql");
 		EntityManager em = fabrica.createEntityManager();
